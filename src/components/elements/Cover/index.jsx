815b23446fe1c6e1c4213fee646953cd9e412f34
DantasLaryssa <dvlaryssa@gmail.com>
2024-01-24 13:47:19 -0300

    github pages

--- src/components/elements/Cover/index.jsx
+++ src/components/elements/Cover/index.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import "./styles.css";
 import picportfolio from "../../../assets/fotoportfolio1.png";
+import picfrontend from "../../../assets/Frontendpic.png";
+import uxpic from "../../../assets/Uxpic.png";
+import cbpic from "../../../assets/codigodebarra.png";
 
 const Cover = React.forwardRef((props, ref) => {
   return (
     <div ref={ref}>
       <div className="cover">
         <img src={picportfolio} alt="Capa" className="picportfolio" />
+        <img src={picfrontend} alt="frontend" className="picfrontend" />
+        <img src={uxpic} alt="ux/ui" className="uxpic" />
+        <a href="https://www.linkedin.com/in/dantaslaryssa/">
+          <img src={cbpic} alt="codigo de barra" className="cbpic" />
+        </a>
         <div className="content">
           <div>
             <p className="title1">PORTFÓLIO</p>
+            <p className="p1">SÃO PAULO</p>
           </div>
 
           <div>
-            <p className="title2" style={{color : "#fff"}}>LA</p>
-            <p className="title2">RYSSA DANTAS VIEIRA</p>
+            <p className="p2">DESENVOLVIDO por</p>
+            <div>
+              <p className="title2" style={{ color: "#fff" }}>
+                LA
+              </p>
+              <p className="title2">RYSSA DANTAS VIEIRA</p>
+            </div>
+            <p className="p3">Desenvolvedora FRONT-END</p>
+            <p className="p3">& UX/UI Designer</p>
           </div>
         </div>
       </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,19 +2,33 @@ import React from "react";
 import HTMLFlipBook from "react-pageflip";
 import Cover from "../../components/elements/Cover";
 import "./styles.css";
+import picverse from "../../assets/fotoverso.jpg";
+
+const Picverse = React.forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <div className="picverse">
+        <img src={picverse} alt="picverse" className="picverse"></img>
+      </div>
+    </div>
+  );
+});
 
 const Home = () => {
   return (
     <div className="home">
-      <div style={{width : "60vw", height : "95vh"  }}>
-        <HTMLFlipBook width={270} height={370} showCover = {true} size = "stretch" >
-            < Cover/>
-            < Cover number = {1}/>
-            < Cover number = {2}/>
-            < Cover number = {3}/>
-
-
- 
+      <div style={{ width: "60vw", height: "95vh" }}>
+        <HTMLFlipBook width={270} height={370} showCover={false} size="stretch">
+          <div  className="picverse" style={{backgroundColor:"blue"}}>teste</div>
+          <Cover />
+          <Picverse />
+          <div  className="picverse" style={{backgroundColor:"blue"}}>teste</div>
+          <div>Pag. 2</div>
+          <div>Pag. 3</div>
+          <div>Pag. 4</div>
+          <div>Pag. 5</div>
+          <div>Pag. 6</div>
+          <div>Pag. 7</div>
         </HTMLFlipBook>
       </div>
     </div>
